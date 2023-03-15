a494997ad907d86a2b5ec35c7f42c8dbb9bfc8be
GraceGenuardi <graziagenuardi96@gmail.com>
2023-03-15 10:52:43 +0100

    FIX img FUTURE

--- proj-html-vuejs/src/components/Future.vue
+++ proj-html-vuejs/src/components/Future.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="future">
 
-        <div class="col-6 ">
+        <div class="col-5 ">
             <h2 class="fs-1 mb-4">Investing for Your Future</h2>
             <p class="fw-lighter lh-5 fs-5">It is no exaggeration to say this MasterStudy experience was transformative–both professionally and personally. This workshop will long remain a high point of my life. Thanks again…. I am feeling energized and eager to start teaching my class next week. I can't wait to use all of my new teaching tools. I will absolutely recommend this workshop to other educators!</p>
             <h6 class="fs-4 mt-5">Linda Green</h6>
@@ -28,15 +28,17 @@
     background-image: url(../assets/img/testimonial-1-1917x640-1-1914x639.jpg);
     background-size: contain;
     background-repeat: no-repeat;
-    padding: 200px;
+    padding-top: 200px;
+    padding-bottom: 500px;
   
   }
   
-  .col-6 {
+  .col-5 {
     background-color: white;
-    padding: 100px;
+    padding: 80px;
     margin-top:-150px;
-    margin-left: 150px;
+    margin-left: 300px;
+    
   }
 
   
